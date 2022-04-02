62b5629af31bd6bcef174f9c8ec192b21cdf8e7c
golub97 <48694046+golub97@users.noreply.github.com>
2022-04-02 21:30:27 +0200

    Time Complexity O(n)

---
+++ 1_Arrays_and_Strings/1.3/URLify.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void replaceSpaces(string &str, int trueLength)
+{
+    int number_of_spaces = 0;
+    int index = 0;
+
+    for(int i = 0; i < trueLength; ++i)
+        if(' ' == str[i])
+            number_of_spaces++;
+            
+    index = trueLength + 2*number_of_spaces;
+    
+    for(int i = trueLength - 1; i >= 0; --i)
+    {
+        cout<< "character " << str[i] << " before =>" << str << endl;
+        if(str[i] == ' ')
+        {
+            str[index - 1] = '0';
+            str[index - 2] = '2';
+            str[index - 3] = '%';
+            index-=3;
+        
+        }
+        else
+        {
+            str[index - 1] = str[i];
+            --index;
+        }
+        cout<< "character " << str[i] << " after  =>" << str << endl;
+    }
+}
+
+int main()
+{
+    string s1 = "Mr John Smith    ";
+    replaceSpaces(s1, 13);
+    cout << s1;
+    
+    return 0;
+}
