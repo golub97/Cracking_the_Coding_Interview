183316071b0a412da03e545c65fa580b3d3a2755
golub97 <48694046+golub97@users.noreply.github.com>
2022-04-21 20:39:31 +0200

    Solution #1.

---
+++ 5_Bit_Manipulation/5.6/conversion.cpp
@@ -0,0 +1,27 @@
+//Conversion: Write a function to determine the number of bits you would need to flip to convert integer A to integer B.
+//EXAMPLE
+//Input: 29 (or: 11101), 15 (or: 01111)
+//Output: 2 
+#include <iostream>
+using namespace std;
+
+int bitSwapRequired(int a, int b)
+{
+    int count = 0;
+    for(int c = a ^ b; c != 0; c >>= 1)
+        count += c & 1;
+        
+    return count;
+}
+
+int main()
+{
+    int a = 29;
+    int b = 15;
+    
+    int result = bitSwapRequired(a, b);
+    
+    cout << result;
+
+    return 0;
+}
